64e04dff4967a14e304313a9225359c1364bce29
Valuur <valuur@googlemail.com>
2011-03-24 16:00:15 +0100

    - Oh my gosh. Here is the error :)

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "";
-$config['cookie_domain']	= ".eternal-knighst.net";
+$config['cookie_domain']	= ".eternal-knights.net";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
