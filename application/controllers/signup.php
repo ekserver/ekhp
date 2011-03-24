774572812b630ac724b7067c585a2e3b764849a4
Valuur <valuur@googlemail.com>
2011-03-24 20:18:20 +0100

    - Some work on signup controller

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -2,8 +2,6 @@
 
 class Signup extends CI_Controller {
 
-    private $data;
-
     function __construct()
     {
         parent::__construct();
@@ -15,43 +13,34 @@ class Signup extends CI_Controller {
         $this->template['layout'] = 'default';
         $this->template['content'] = 'signup';
         $this->load->view('template', $this->template);
-        
-        // Load form_validation library & set error delimiters
-        $this->load->library('form_validation');
     }
     
     function signup()
     {
+        // Load form_validation library
+        $this->load->library('form_validation');
+        
         $this->form_validation->set_rules('email', 'Email-Adresse', 'trim|required|valid_email|callback__check_email');
         $this->form_validation->set_rules('username', 'Benutzername', 'trim|required|min_length[4]|max_length[12]|callback__check_username');
         $this->form_validation->set_rules('password', 'Passwort', 'trim|required|min_length[4]|max_length[32]');
         $this->form_validation->set_rules('password2', 'Passwort wiederholen', 'trim|required|matches[password]');
+        $this->form_validation->set_rules('expansion', 'Erweiterung', 'trim|required');
         $this->form_validation->set_rules('ip', 'Ip-Adresse', 'trim|required|callback__check_ip');
         
-        /*$this->form_validation->set_rules('firstname', 'Vorname', 'trim|min_length[2]|max_length[12]');
+        $this->form_validation->set_rules('firstname', 'Vorname', 'trim|min_length[2]|max_length[12]');
         $this->form_validation->set_rules('lastname', 'Nachname', 'trim|min_length[3]|max_length[12]');
-        $this->form_validation->set_rules('age', 'Alter', 'trim|required');
-        $this->form_validation->set_rules('country', 'Land', 'trim|required');
-        $this->form_validation->set_rules('state', 'Bundesland', 'trim');
-        $this->form_validation->set_rules('city', 'Stadt', 'trim');*/
         
         if($this->form_validation->run() == TRUE)
         {
-            $data_game          = array(
+            $data               = array(
                 'username'      => $this->input->post('username'),
                 'email'         => $this->input->post('email'),
                 'sha_pass_hash' => sha_pass($this->input->post('password')),
-                'expansion'     => $this->input->post('expansion')
-                );
-                
-            /*$data_personal  = array(
-                'firstname' => $this->input->post('firstname');
-                'lastname'  => $this->input->post('lastname');
-                'age'       => $this->input->post('age');
-                'country'   => $this->input->post('country');
-                'state'     => $this->input->post('state');j
-                'city'      => $this->input->post('city');
-                );*/
+                'expansion'     => $this->input->post('expansion'),
+
+                'firstname'     => $this->input->post('firstname'),
+                'lastname'      => $this->input->post('lastname')
+            );
             
             if($this->user->register($data_game))
             {
@@ -59,6 +48,8 @@ class Signup extends CI_Controller {
             }
             return false;
         }
+        else
+            return false;
     }
     
     private function _check_username($username)
@@ -114,18 +105,16 @@ class Signup extends CI_Controller {
             if($bandate == $unbandate)
             {
                 $this->form_validation->set_message('check_ip', 'Deine IP %s wurde permanent gebannt! Grund: '.$reason.'.! Du kannst leider nicht mehr am Projekt teilnehmen :(');
-                return false;
             }
             elseif($bandate != $unbandate && (now() > $unbandate)) // If user was already banned but ban is not active anymore, user gets message that ip is free and user can login again
             {
                 $this->form_validation->set_message('check_ip', 'Deine IP %s war bereits vom '.unix_to_human($bandate, TRUE, "eu").' bis '. unix_to_human($unbandate, TRUE, "eu").' gebannt! Du kannst dich nun wieder einloggen! Doppelaccounts sind NICHT m&ouml;glich!');
-                return false;
             }
             else
             {
                 $this->form_validation->set_message('check_ip', 'Deine IP %s ist noch bis '.unix_to_human($unbandate, TRUE, "eu").' gebannt und kannst dich erst dann wieder einloggen! Doppelaccounts sind NICHT m&ouml;glich!'); 
-                return false;
             }
+            return false;
         }
         else
             return true;
